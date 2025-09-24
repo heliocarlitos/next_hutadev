de1c9113d872b3f27f9e44a777954e0050eac644
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2025-09-24 14:12:56 +0200

    Adicionei um efeito de hover nas tecnologias usadas.

--- src/components/herohome/herohome.tsx
+++ src/components/herohome/herohome.tsx
@@ -1,81 +1,79 @@
 import { BsStars } from "react-icons/bs";
 import Image from "next/image";
 import {
-    IoLogoHtml5,
-    IoLogoFigma,
-    IoLogoReact,
-    IoLogoFirebase,
+  IoLogoHtml5,
+  IoLogoFigma,
+  IoLogoReact,
+  IoLogoFirebase,
 } from "react-icons/io5";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiTypescript } from "react-icons/si";
 import "./herohome.css";
 export function HeroHome() {
-    return (
-        <>
-            <div className="herohome">
-                <p className="title gradient-text">
-                    Resultados mensuráveis em design, desenvolvimento e
-                    marketing.
-                </p>
+  return (
+    <>
+      <div className="herohome">
+        <p className="title gradient-text">
+          Resultados mensuráveis em design, desenvolvimento e marketing.
+        </p>
 
-                <p className="text">
-                    Nossa missão é simplificar as complexidades das startups
-                    modernas de negócios e tecnologia, permitindo que nossos
-                    clientes se concentrem no que realmente importa - seu
-                    crescimento e sucesso.
-                </p>
+        <p className="text">
+          Nossa missão é simplificar as complexidades das startups modernas de
+          negócios e tecnologia, permitindo que nossos clientes se concentrem no
+          que realmente importa - seu crescimento e sucesso.
+        </p>
 
-                <button>
-                    Começar agora
-                    <div className="icon">
-                        <BsStars />
-                    </div>
-                </button>
+        <button>
+          Começar agora
+          <div className="icon">
+            <BsStars />
+          </div>
+        </button>
 
-                <div className="tecnology">
-                    <div className="icon">
-                        <IoLogoHtml5 />
-                    </div>
-                    <div className="icon">
-                        <RiTailwindCssFill />
-                    </div>
+        <div className="tecnology">
+          <div className="icon" data-text="HTML5">
+            <IoLogoHtml5 />
+          </div>
+          <div className="icon" data-text="Tailwind">
+            <RiTailwindCssFill />
+          </div>
 
-                    <div className="icon">
-                        <IoLogoFigma />
-                    </div>
+          <div className="icon" data-text="Figma">
+            <IoLogoFigma />
+          </div>
 
-                    <div className="icon">
-                        <IoLogoReact />
-                    </div>
+          <div className="icon" data-text="React">
+            <IoLogoReact />
+          </div>
 
-                    <div className="icon">
-                        <SiTypescript />
-                    </div>
+          <div className="icon" data-text="TypeScript">
+            <SiTypescript />
+          </div>
 
-                    <div className="icon">
-                        <IoLogoFirebase />
-                    </div>
-                </div>
+          <div className="icon" data-text="Firebase">
+            <IoLogoFirebase />
+          </div>
+        </div>
 
-                <div className="banner">
-                    <figure>
-                        <img
-                            src="/image/relative/dark.png"
-                            alt="capa dark"
-                            width={980}
-                            height={600}
-                            className="dark"
-                        />
-                        <img
-                            src="/image/relative/light.png"
-                            alt="capa light"
-                            width={980}
-                            height={600}
-                            className="light"
-                        />
-                    </figure>
-                </div>
-            </div>
-        </>
-    );
+        <div className="banner">
+          <figure>
+            <img
+              src="/image/relative/dark.png"
+              alt="capa dark"
+              width={980}
+              height={600}
+              className="dark"
+            />
+            <img
+              src="/image/relative/light.png"
+              alt="capa light"
+              width={980}
+              height={600}
+              className="light"
+            />
+          </figure>
+        </div>
+      </div>
+    </>
+  );
 }
