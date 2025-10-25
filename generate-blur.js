159fbcfa904958681d822fad1099ad27f106850e
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2025-10-26 00:54:24 +0200

    Adicionei uma blurDataURL

---
+++ generate-blur.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const imgPath = path.join(process.cwd(), "public/image/relative/hero-small.png");
+
+try {
+  const img = fs.readFileSync(imgPath);
+  const base64 = img.toString("base64");
+  console.log(`data:image/png;base64,${base64}`);
+} catch (error) {
+  console.error("Erro ao ler a imagem:", error);
+}
--- src/components/herohome/herohome.tsx
+++ src/components/herohome/herohome.tsx
@@ -1,5 +1,6 @@
 import { BsStars } from "react-icons/bs";
 import Link from "next/link";
+import Image from "next/image";
 import {
   IoLogoHtml5,
   IoLogoFigma,
@@ -13,7 +14,6 @@ export function HeroHome() {
   return (
     <>
       <div className="herohome">
-
         <p className="title">
           Resultados mensuráveis em design, desenvolvimento e marketing.
         </p>
@@ -34,9 +34,9 @@ export function HeroHome() {
               <BsStars />
             </div>
           </button>
-          
+
           <Link href="#" className="who-are">
-           <div className="text-area">
+            <div className="text-area">
               <div className="text">Sobre nós</div>
               <div className="text">Sobre nós</div>
             </div>
@@ -70,21 +70,25 @@ export function HeroHome() {
 
         <div className="banner">
           <figure>
-            <img
+            <Image
               src="/image/relative/dark.webp"
               alt="capa dark"
               width={980}
               height={600}
               className="dark"
               fetchPriority="high"
+              placeholder="blur"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAKCAMAAACDi47UAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAADnUExURf////v9/u/y9Pv7+vj4//X1/fv7+/r6+vz8/Ozs7aOhpOrp6v7///3+/v39/fLy8/Dw8vPz8/T09PLy8vHx8eHh4dDQ0NTV1dPT09bW19XV1dDQ0ePj4/7+/u/v7/Dw8PTz9O3t7e7u7vX19f7+/87G57Wq2r203vTz+vr6+dPR2srI1c/M2Nzc3uDg4Pn6+fn5+eTk5OLj4uLi4ubm5uHh4/Py9//+//z6/vb0+f37/vv7/vr6/fHs+t/Z8/f6/f3+///8//Tp9+rh7+jh7+zn9fTy//L0/uLO++KO+evR/On1/vn8/gRFWRwAAAABYktHRACIBR1IAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAB3RJTUUH6QoZFik1kGBkVAAAAIlJREFUCNdVyNUOwjAARuEfa4sVOhxGB8OdoYPhbu//PGQJId25Ox8Anz+AYIhQRghBOBKFWywOnkgKIbSUBpZm+MUz2Vy+UCzpZShRKaUhJVUNrFI1TVljHqw3mq12h3sMtNvrD/ThyINja2JNZ/OFQkt7tbYdZ7Pd7Q9/PJ7Ol+vt/ni+3h/3v642DX5t34KOAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDI1LTEwLTI1VDIyOjQxOjAwKzAwOjAwUj6O/wAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyNS0xMC0yNVQyMjo0MTowMCswMDowMCNjNkMAAAAodEVYdGRhdGU6dGltZXN0YW1wADIwMjUtMTAtMjVUMjI6NDE6NTMrMDA6MDANfgNlAAAAAElFTkSuQmCC"
             />
-            <img
+            <Image
               src="/image/relative/light.webp"
               alt="capa light"
               width={980}
               height={600}
               className="light"
               fetchPriority="high"
+              placeholder="blur"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAKCAMAAACDi47UAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAADnUExURf////v9/u/y9Pv7+vj4//X1/fv7+/r6+vz8/Ozs7aOhpOrp6v7///3+/v39/fLy8/Dw8vPz8/T09PLy8vHx8eHh4dDQ0NTV1dPT09bW19XV1dDQ0ePj4/7+/u/v7/Dw8PTz9O3t7e7u7vX19f7+/87G57Wq2r203vTz+vr6+dPR2srI1c/M2Nzc3uDg4Pn6+fn5+eTk5OLj4uLi4ubm5uHh4/Py9//+//z6/vb0+f37/vv7/vr6/fHs+t/Z8/f6/f3+///8//Tp9+rh7+jh7+zn9fTy//L0/uLO++KO+evR/On1/vn8/gRFWRwAAAABYktHRACIBR1IAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAB3RJTUUH6QoZFik1kGBkVAAAAIlJREFUCNdVyNUOwjAARuEfa4sVOhxGB8OdoYPhbu//PGQJId25Ox8Anz+AYIhQRghBOBKFWywOnkgKIbSUBpZm+MUz2Vy+UCzpZShRKaUhJVUNrFI1TVljHqw3mq12h3sMtNvrD/ThyINja2JNZ/OFQkt7tbYdZ7Pd7Q9/PJ7Ol+vt/ni+3h/3v642DX5t34KOAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDI1LTEwLTI1VDIyOjQxOjAwKzAwOjAwUj6O/wAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyNS0xMC0yNVQyMjo0MTowMCswMDowMCNjNkMAAAAodEVYdGRhdGU6dGltZXN0YW1wADIwMjUtMTAtMjVUMjI6NDE6NTMrMDA6MDANfgNlAAAAAElFTkSuQmCC"
             />
           </figure>
         </div>
