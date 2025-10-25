b121912d67b3888bc143a06ae3365f29bb94fcdf
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2025-10-25 13:56:39 +0200

    ...

--- src/components/cumpany/page.tsx
+++ src/components/cumpany/page.tsx
@@ -6,23 +6,22 @@ export function Cumpany() {
       <div className="cumpany">
 
         <div className="intro-logo">
-          <p className="title">Confiados por empresas de renome no mercado</p>
-       
+          <p className="title">Nossos serviços e produtos foram usados ​​por pessoas dessas empresas em seus projectos.</p>
         </div>
 
         <div className="logos">
           <figure>
             <img src="image/logo/lam-logo.webp" alt="Logo" width={150} height={40} loading="lazy"/>
           </figure>
-          
+
           <figure>
             <img src="image/logo/cfm-logo.webp" alt="Logo" width={150} height={40} loading="lazy"/>
           </figure>
-          
+
           <figure>
             <img src="image/logo/global-logo.webp" alt="Logo" width={150} height={40} loading="lazy"/>
           </figure>
-          
+
           <figure>
             <img src="image/logo/mozrural-logo.webp" alt="Logo" width={150} height={40} loading="lazy"/>
           </figure>
@@ -32,3 +31,4 @@ export function Cumpany() {
     </>
   );
 }
+
